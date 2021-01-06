6a5d6f393ca99e44a7f59cc09c8b62f378beaca5
Tej Pratap <kumarpratap.1447@gmail.com>
2021-01-07 01:55:16 +0530

    font page modified

--- src/layout/Features.js
+++ src/layout/Features.js
@@ -4,6 +4,35 @@ const Features = () => {
     return (
         <div>
            <h1>Features</h1> 
+           <div class="row">
+
+           <div class="shadow p-3 mb-5 bg-white rounded col-sm-6">
+           
+           <h3><i class="fas fa-blog icon-home"></i><span>Blogs</span></h3>
+           <p>Write Your Thoughts share with all. choose between three categories
+           Tech, Non-Tech or Confess. Write some useful content to Help Others.</p>
+           </div>
+
+           <div class="shadow p-3 mb-5 bg-white rounded col-sm-6">
+           <h3><i class="fas fa-book-open icon-home"></i><span>Academics</span></h3>
+           <p>Write Your Thoughts share with all. choose between three categories
+           Tech, Non-Tech or Confess. Write some useful content to Help Others.</p>
+           </div>
+
+           <div class="shadow p-3 mb-5 bg-white rounded col-sm-6">
+           <h3><i class="fas fa-users icon-home"></i><span>Social</span></h3>
+           <p>Write Your Thoughts share with all. choose between three categories
+           Tech, Non-Tech or Confess. Write some useful content to Help Others.</p>
+           </div>
+
+           <div class="shadow p-3 mb-5 bg-white rounded col-sm-6">
+           <h3><i class="fas fa-shopping-cart icon-home"></i><span>Shop</span></h3>
+           <p>Write Your Thoughts share with all. choose between three categories
+           Tech, Non-Tech or Confess. Write some useful content to Help Others.</p>
+           </div>
+
+           </div>
+           
         </div>
     )
 }
